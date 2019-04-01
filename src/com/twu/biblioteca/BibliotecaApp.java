fcf851012d07280a15a754dbe82bbc941785edab
rosie amphlett <rosieamphlett@hotmail.co.uk>
2019-04-01 20:34:40 +0100

    welcome message, and showing all books with tests

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,31 @@
 package com.twu.biblioteca;
 
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp bibliotecaApp = new BibliotecaApp();
+        bibliotecaApp.welcome();
+        bibliotecaApp.showAllBooks();
+    }
+
+    public String welcome() {
+        String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        System.out.println(welcomeMessage + "\n");
+        return welcomeMessage;
+    }
+
+    public List showAllBooks() {
+        Books library = new Books();
+        List<String> books = library.getBooks();
+        System.out.println(books);
+
+        for (String book : books) {
+            System.out.println(book);
+        }
+        return books;
     }
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,24 @@
 package com.twu.biblioteca;
 
-
 import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
+    private BibliotecaApp test = new BibliotecaApp();
+    private Books testBooks = new Books();
 
-    @Test
-    public void test() {
-        assertEquals(1, 1);
+    @Test public void testShowsWelcomeMessage() {
+        String expected = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        String actual = test.welcome();
+        assertEquals(expected, actual);
+    }
+
+    @Test public void testShowsAllBooks() {
+        List<String> expected = testBooks.getBooks();
+        List<String> actual = test.showAllBooks();
+        assertEquals(expected, actual);
     }
 }
